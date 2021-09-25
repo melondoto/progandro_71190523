1568180aeac117b5c87e10125a13ed0b2f07eaf7
melondoto <revyn.pradana@ti.ukdw.ac.id>
2021-09-25 23:53:19 +0700

    tambah pertemuan4

---
+++ pert4_71190523/app/src/main/java/com/example/pert4_71190523/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.pert4_71190523
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
